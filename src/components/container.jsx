c7b21cfbfbd6d7cc17a0878990f8015c587c8f0c
iraklitsikhelishvilii <11cixela11@gmail.com>
2025-01-12 00:04:47 +0400

    feat(PROJECT_STRUCTURE):added structure

---
+++ src/components/container.jsx
@@ -0,0 +1,14 @@
+import Foto from "../assets/result.png";
+
+function container() {
+  return (
+    <>
+      <div className="container">
+        <p>hello world</p>
+        <img src={Foto} alt="" />
+      </div>
+    </>
+  );
+}
+
+export default container;
